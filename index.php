2a56cd30caca37f28239eec90bec3a7b764bdbf6
Carlos-Henriquez <202010203@itla.edu.do>
2022-11-26 04:11:57 -0400

    Desarrollo: titulo y creacion de la tabla en el index. CH

--- index.php
+++ index.php
@@ -6,3 +6,41 @@ if (is_dir("DB") == false) {
     mkdir("BD");
 }
 ?>
+
+<title>Robos RD</title>
+
+<div class="container">
+    <div style="text-align:center;">
+        <h4><b>Lista de robos</b></h4>
+    </div>
+
+    <table class="highlight">
+        <thead>
+            <tr>
+                <th>Cédula</th>
+                <th>|</th>
+                <th>Nombre</th>
+                <th>|</th>
+                <th>Fecha</th>
+                <th>|</th>
+                <th>¿Qué se robaron?</th>
+                <th>|</th>
+                <th>Valor perdido</th>
+                <th>|</th>
+                <th>¿Dónde ocurrió?</th>
+                <th>|</th>
+                <th>Latitud</th>
+                <th>Longitud</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+</div>
+
+<?php
+/*
+Carlos Henriquez
+20202-10203
+*/
